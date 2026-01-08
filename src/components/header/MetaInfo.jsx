4508a628a2586a442a857e0d67d92a86579965cf
Justin Marley Fontanilla <71302306+jfontz@users.noreply.github.com>
2026-01-08 23:02:38 +0800

    refactor(header): extract MetaInfo component

---
+++ src/components/header/MetaInfo.jsx
@@ -0,0 +1,9 @@
+const MetaInfo = ({ className }) => (
+  <div className={className}>
+    <span>192.168.254.254</span>
+    <span>•</span>
+    <span>Last updated: 12s ago</span>
+  </div>
+);
+
+export default MetaInfo;
