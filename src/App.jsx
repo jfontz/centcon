4858e4d781b01f6ab066b6bb1221878ad9e0cfd0
Justin Marley Fontanilla <71302306+jfontz@users.noreply.github.com>
2026-01-12 21:56:57 +0800

    feat(App): integrate ModemProvider for modem state management and restructure layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,47 @@
-// src/App.jsx (or Dashboard.jsx)
 import MainLayout from "./components/ui/MainLayout";
-import SectionContainer from "./components/ui/SectionContainer";
 import SystemControls from "./components/SystemControls";
 import SystemStatus from "./components/SystemStatus";
 import DeviceInformation from "./components/DeviceInformation";
 import LogPanel from "./components/LogPanel";
 import ConnectedDevices from "./components/ConnectedDevices";
+import { ModemProvider } from "./context/ModemContext";
 
 function App() {
   return (
-    <MainLayout>
-      {/* The Main Grid 
+    <ModemProvider refreshInterval={10000}>
+      <MainLayout>
+        {/* The Main Grid 
          - On Mobile: 1 column
          - On Desktop (lg): 12 columns. 
            Left side takes 7 or 8 columns. Right side takes the rest.
       */}
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-        {/* LEFT COLUMN (Controls + Status) */}
-        <div className="lg:col-span-8 flex flex-col gap-6">
-          {/* Top: System Controls */}
-          <SystemControls />
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+          {/* LEFT COLUMN (Controls + Status) */}
+          <div className="lg:col-span-8 flex flex-col gap-6">
+            {/* Top: System Controls */}
+            <SystemControls />
 
-          <div className="flex flex-col lg:flex-row w-full gap-5 items-stretch">
-            <div className="flex-2">
-              <SystemStatus />
+            <div className="flex flex-col lg:flex-row w-full gap-5 items-stretch">
+              <div className="flex-2">
+                <SystemStatus />
+              </div>
+
+              <div className="flex-1">
+                <DeviceInformation />
+              </div>
             </div>
 
-            <div className="flex-1">
-              <DeviceInformation />
-            </div>
+            {/* Bottom: Connected Devices */}
+            <ConnectedDevices />
           </div>
 
-          {/* Bottom: Connected Devices */}
-          <ConnectedDevices />
-        </div>
-
-        {/* RIGHT COLUMN (Log) */}
-        <div className="lg:col-span-4 h-full">
-          <SectionContainer
-            title="Log"
-            className="h-full min-h-125 fixed top-0"
-          >
+          {/* RIGHT COLUMN (Log) */}
+          <div className="lg:col-span-4 h-full flex flex-col min-h-0">
             <LogPanel />
-          </SectionContainer>
+          </div>
         </div>
-      </div>
-    </MainLayout>
+      </MainLayout>
+    </ModemProvider>
   );
 }
 
