ebb620d100c9fa7f40849fb873b2ce031a76dc2f
Justin Marley Fontanilla <71302306+jfontz@users.noreply.github.com>
2026-01-22 20:32:49 +0800

    feat: add dynamic modem IP and live "last updated" time to MetaInfo

--- src/components/header/MetaInfo.jsx
+++ src/components/header/MetaInfo.jsx
@@ -1,9 +1,29 @@
-const MetaInfo = ({ className }) => (
-  <div className={className}>
-    <span>192.168.254.254</span>
-    <span>•</span>
-    <span>Last updated: 12s ago</span>
-  </div>
-);
+import { useState, useEffect } from "react";
+import { formatTimeAgo } from "../../utils/formatters";
+
+const MetaInfo = ({ className, lastUpdated }) => {
+  const modemIp = import.meta.env.VITE_MODEM_IP || "192.168.254.254";
+  const [timeAgo, setTimeAgo] = useState(formatTimeAgo(lastUpdated));
+
+  useEffect(() => {
+    setTimeAgo(formatTimeAgo(lastUpdated));
+  }, [lastUpdated]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeAgo(formatTimeAgo(lastUpdated));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [lastUpdated]);
+
+  return (
+    <div className={className}>
+      <span>{modemIp}</span>
+      <span>•</span>
+      <span>Last updated: {timeAgo}</span>
+    </div>
+  );
+};
 
 export default MetaInfo;
