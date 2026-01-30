2614544a3f9c818a743b4af0b6b781efc07bcb48
Justin Marley Fontanilla <71302306+jfontz@users.noreply.github.com>
2026-01-30 15:58:01 +0800

    refactor: replace DeviceSerialCard with DeviceFirmwareCard

--- src/components/cards/DeviceFirmwareCard.jsx
+++ src/components/cards/DeviceFirmwareCard.jsx
@@ -1,14 +1,14 @@
 import MetricCard from "../ui/MetricCard";
 import IconWrapper from "../ui/IconWrapper";
-import { serial } from "../../assets/icons";
+import { firmware } from "../../assets/icons";
 
 const DeviceSerialCard = ({ value, loading }) => {
   const displayValue = value || (loading ? "..." : "N/A");
 
   return (
     <MetricCard
-      icon={<IconWrapper src={serial} alt="Model serial" />}
-      label="Model Serial"
+      icon={<IconWrapper src={firmware} alt="Firmware Version" />}
+      label="Firmware Version"
       value={displayValue}
     />
   );
