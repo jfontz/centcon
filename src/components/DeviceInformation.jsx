6202e4a4b5d322216648b438cad862f940923fae
Justin Marley Fontanilla <71302306+jfontz@users.noreply.github.com>
2026-01-30 15:59:59 +0800

    refactor: replace DeviceSerialCard with DeviceFirmwareCard

--- src/components/DeviceInformation.jsx
+++ src/components/DeviceInformation.jsx
@@ -1,6 +1,6 @@
 import SectionContainer from "./ui/SectionContainer";
 import DeviceModelCard from "./cards/DeviceModelCard";
-import DeviceSerialCard from "./cards/DeviceSerialCard";
+import DeviceFirmwareCard from "./cards/DeviceFirmwareCard";
 import { useModem } from "../context/ModemContext";
 
 const DeviceInformation = () => {
@@ -12,7 +12,7 @@ const DeviceInformation = () => {
     <SectionContainer title="Device Information" className="w-full h-full">
       <div className="grid grid-cols-1 gap-4">
         <DeviceModelCard value={deviceInfo?.model} loading={loading} />
-        <DeviceSerialCard value={deviceInfo?.serial} loading={loading} />
+        <DeviceFirmwareCard value={deviceInfo?.software} loading={loading} />
       </div>
     </SectionContainer>
   );
